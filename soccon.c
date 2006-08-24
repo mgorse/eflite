3bc0e81fa944420058652d69d886e3d7f920a345
Mike Gorse <mgorse@alum.wpi.edu>
2006-08-24 11:37:53 +0000

    Add support for tcp connections

--- soccon.c
+++ soccon.c
@@ -1,14 +1,17 @@
 /* soccon.c - function to open a connection to a socket
- * $Id$
+ * $Id: soccon.c,v 1.2 2002/03/06 02:25:18 mgorse Exp $
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <sys/un.h>
 #include <unistd.h>
 
-int sockconnect(const char *fname)
+int sockconnect_unix(const char *fname)
 {
   struct sockaddr_un addr;
   int sock;
@@ -28,4 +31,56 @@ int sockconnect(const char *fname)
     return -1;
   }
   return sock;
+}
+
+static int sockconnect_tcp(const char *port)
+{
+  char host[1024];
+  int portnum;
+  int fd;
+  struct sockaddr_in servaddr;
+  char *p;
+  int len;
+
+  p = strstr(port, ":");
+  portnum = atoi(p + 1);
+  len = p - port;
+  if (len > 1023)
+  {
+    fprintf(stderr, "syntheport long\n");
+    exit(1);
+  }
+  memcpy(host, port, len);
+  host[len] = '\0';
+  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    perror("socket");
+    exit(1);
+  }
+  memset(&servaddr, 0, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_port = htons(portnum);
+
+#ifdef HAVE_INET_PTON
+  if (inet_pton(AF_INET, host, &servaddr.sin_addr) <= 0)
+  {
+    perror("inet_pton");
+    exit(1);
+  }
+#else
+  servaddr.sin_addr.s_addr = inet_addr(host);
+#endif
+
+  if (connect(fd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
+  {
+    return -1;
+  }
+  return fd;
+}
+
+int sockconnect(const char *name)
+{
+  if (!name) return -1;
+  if (strchr(name, ':')) return sockconnect_tcp(name);
+  else return sockconnect_unix(name);
 }
--- sockopen.c
+++ sockopen.c
@@ -2,14 +2,17 @@
  * $id$
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <sys/stat.h>
 #include <sys/un.h>
 #include <syslog.h>
 
-int sockopen(const char *fname)
+int sockopen_unix(const char *fname)
 {
   int sock;
   struct sockaddr_un addr;
@@ -32,4 +35,41 @@ int sockopen(const char *fname)
     return -1;
   }
   return sock;
+}
+
+int sockopen_tcp(const char *name)
+{
+  int port;
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in serv_addr;
+  const char *p;
+
+  p = strchr(name, ':');
+  if (!p)
+  {
+    fprintf(stderr, "Argh! Internal error!\n");
+    exit(1);
+  }
+  port = atoi(p + 1);
+  memset(&serv_addr, 0, sizeof(struct sockaddr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(port);
+  if (bind(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+  {
+    fprintf(stderr, "Error in binding to port %d.\n", port);
+    exit(1);
+  }
+  if (listen(sock, 5) == -1)
+  {
+    perror("listen");
+    exit(1);
+  }
+  return sock;
+}
+int sockopen(const char *name)
+{
+  if (!name) return -1;
+  if (strchr(name, ':')) return sockopen_tcp(name);
+  else return sockopen_unix(name);
 }
